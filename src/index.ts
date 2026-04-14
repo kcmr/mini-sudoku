c8a96a132cedcfb1764775bb0a985c04484a15e8
Kus Cámara <kcmr@users.noreply.github.com>
2026-04-14 22:53:35 +0200

    fix: prevent editing readonly cells

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-#!/usr/bin/env nodeimport { getGrid } from './engine.js';
+#!/usr/bin/env node
+
 import { arrayRange, getGrid, removeCells } from './engine.js';
 import { render } from './ui.js';
 import readline from 'readline';
@@ -7,10 +8,10 @@ readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
 const cursor = {x: 0, y: 0}
-const grid = getGrid()
-const incompleteGrid = removeCells(grid)
+const initialGrid = removeCells(getGrid())
+const grid = structuredClone(initialGrid)
 
-render(incompleteGrid, cursor);
+render(grid, cursor);
 
 
 process.stdin.on('keypress', (str, key) => {
@@ -23,14 +24,16 @@ process.stdin.on('keypress', (str, key) => {
   if (key.name === 'left' && cursor.x > 0) cursor.x--;
   if (key.name === 'right' && cursor.x < 5) cursor.x++;
 
+  const isEditableCell = initialGrid[cursor.y][cursor.x] === 0
   const allowedNumbers = arrayRange(1, 6).map((n) => String(n))
+
   
   if (allowedNumbers.includes(key.name)) {
-    incompleteGrid[cursor.y][cursor.x] = parseInt(key.name);
+    if (isEditableCell) grid[cursor.y][cursor.x] = parseInt(key.name);
   } else if (key.name && key.name.match(/^[a-z0-9]$/i)) {
     // Si pulsa cualquier otra letra o número no válido (ej: 'e', '7', '9')
     errorMessage = `La tecla ${key.name} no es válida. Usa números del 1 al 6.`
   }
 
-  render(incompleteGrid, cursor, errorMessage);
+  render(grid, cursor, errorMessage);
 });
